b9519b220059810001a18395b3c2628258556812
tubs <tubs33959@gmail.com>
2020-10-17 12:06:10 +0800

    Created API for select query

---
+++ server/controller/query.js
@@ -0,0 +1,181 @@
+const express = require('express')
+const mssql = require('mssql')
+const config = require('../config/db.config')()
+
+
+const router = express.Router()
+
+// =====================================================================
+// ======================= Select Query (MSSQL)=========================
+// =====================================================================
+router.get('/company', (req, res) => {
+     config.connect().then(() => {
+         const request = new mssql.Request(config)
+         request.query(`SELECT ShortName FROM Companies`, (err, results) => {
+             if(err) {
+                 res.send(err)
+             } else {
+                 res.send(results.recordset)
+             }
+             config.close()
+         })
+     })
+ })
+
+router.get('/employeeinfo/:code', (req, res) => {
+     let code = req.params.code
+     config.connect().then(() => {
+         const request = new mssql.Request(config)
+         request.query(`SELECT * FROM EmployeeInformationView WHERE EmployeeCode = '${code}'`, (err, results) => {
+             if(err) {
+                 res.send(err)
+             } else {
+                 res.send(results.recordset)
+             }
+             config.close()
+         })
+     })
+ })
+
+router.get('/employees/:company', (req, res) => {
+     let company = req.params.company
+     let sql = `SELECT * FROM EmployeeInformationView WHERE lower(ShortName) = lower('${company}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/employees/:compname/:department', (req, res) => {
+     let compname = req.params.compname
+     let department = req.params.department
+     let sql = `SELECT * FROM EmployeeInformationView WHERE lower(ShortName) = lower('${compname}')
+               AND lower(DepartmentName) = lower('${department}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/employees/:compname/:department/:section', (req, res) => {
+     let compname = req.params.compname
+     let department = req.params.department
+     let section = req.params.section
+     let sql = `SELECT * FROM EmployeeInformationView WHERE lower(ShortName) = lower('${compname}')
+               AND lower(DepartmentName) = lower('${department}')
+               AND lower(SectionName) = lower('${section}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/employees/:compname/:department/:section/:team', (req, res) => {
+     let compname = req.params.compname
+     let department = req.params.department
+     let section = req.params.section
+     let team = req.params.team
+     let sql = `SELECT * FROM EmployeeInformationView WHERE lower(ShortName) = lower('${compname}')
+               AND lower(DepartmentName) = lower('${department}')
+               AND lower(SectionName) = lower('${section}')
+               AND lower(TeamName) = lower('${team}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+
+router.get('/company/department/:compname', (req, res) => {
+     let compname = req.params.compname
+     let sql = `SELECT * FROM DepartmentView WHERE lower(ShortName) = lower('${compname}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/company/department/section/:compname', (req, res) => {
+     let compname = req.params.compname
+     let sql = `SELECT * FROM DepartmentSectionRelationView WHERE lower(ShortName) = lower('${compname}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/company/department/section/team/:compname', (req, res) => {
+     let compname = req.params.compname
+     let sql = `SELECT * FROM SectionTeamRelationView WHERE lower(ShortName) = lower('${compname}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+
+router.get('/company/designation/:compname', (req, res) => {
+     let compname = req.params.compname
+     let sql = `SELECT * FROM DesignationView WHERE lower(ShortName) = lower('${compname}')`
+     config.connect().then(() => {
+          const request = new mssql.Request(config)
+          request.query(sql, (err, results) => {
+               if(err) {
+                    res.send(err)
+               } else {
+                    res.send(results.recordset)
+               }
+               config.close()
+          })
+     })
+})
+ 
+module.exports = router
