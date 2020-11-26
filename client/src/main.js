09062c96eaa7ed73f959701262680a37bbb7384e
tubs <tubs33959@gmail.com>
2020-11-26 10:42:56 +0800

    Added socket in client folder

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,16 @@ import axios from 'axios'
 import md5 from 'md5'
 import swal from 'sweetalert2'
 import moment from 'moment'
+import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import mixins from './plugins/mixins'
+
+export const SocketInstance = socketio('http://localhost:8700');
 
 // Vue Config 
 Vue.config.productionTip = false
@@ -22,6 +26,10 @@ Vue.prototype.moment = moment
 
 // Vue Use
 Vue.use(mixins)
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketInstance
+}));
 
 new Vue({
   router,
--- server/app.js
+++ server/app.js
@@ -15,8 +15,33 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use(helmet())
 
-app.listen(port, () => {
-    console.log(`Server running at ${port}`)
+// app.listen(port, () => {
+//     console.log(`Server running at ${port}`)
+// })
+
+const server = app.listen(port, () => {
+     console.log(`Server running at ${port}`)
+})
+const socketio = require('socket.io')(server, {
+     cors: {
+          origin: process.env.URL, // "http://localhost:1000",
+          methods: ["GET", "POST"]
+     }
+})
+     
+// app.use('/api',query)
+socketio.on('connection', (socket) => {
+     // console.log(socket.id)
+     
+     //User Logged In
+     socket.on('loggedIn', (data) => {
+          socketio.emit('loggedIn', data)
+     })
+
+     //call function for retrieving data
+     socket.on('chatMessage', (data) => {
+          socketio.emit('chatMessage', data)
+     })
 })
 
 app.use('/api',query)
